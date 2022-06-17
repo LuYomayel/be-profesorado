6368da9586b5b8674940b5bab8fc64643aedcaec
LuYomayel <luciano.yomayel@alumnos.frgp.utn.edu.ar>
2022-06-17 02:16:48 -0300

    probando

--- database/config.js
+++ database/config.js
@@ -5,7 +5,7 @@ const dbConnection =  async() => {
         
         await mongoose.connect( process.env.MONGODB_CNN, {
             useNewUrlParser: true,
-            useUnifiedTopology: true,
+            // useUnifiedTopology: true,
             // useCreateIndex: true,
             // useFindAndModify:false,
            
